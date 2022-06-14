1c03b0716278451b4a5bc81d8b6b4c8a2df502a4
denimlp <andytorsbo@live.com>
2022-06-14 15:06:21 +0200

    Todo, can add and delete todos

---
+++ TodoJS.js
@@ -0,0 +1,16 @@
+let AddToDoButton = document.getElementById('addTodo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+AddToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value="";
+
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
+});
+
