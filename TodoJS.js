e0a035ec09d98fe5e4ad80c214809796d8841acd
Aalo29 <sarah.ali247@gmail.com>
2022-06-20 08:39:04 +0200

    uncomment

--- TodoJS.js
+++ TodoJS.js
@@ -1,19 +1,17 @@
-// let AddToDoButton = document.getElementById('addTodo');
-// let toDoContainer = document.getElementById('toDoContainer');
-// let inputField = document.getElementById('inputField');
+let AddToDoButton = document.getElementById("addTodo");
+let toDoContainer = document.getElementById("toDoContainer");
+let inputField = document.getElementById("inputField");
 
-
-// AddToDoButton.addEventListener('click', function(){
-//     var paragraph = document.createElement('p');
-//     paragraph.classList.add('paragraph-styling');
-//     paragraph.innerText = inputField.value;
-//     toDoContainer.appendChild(paragraph);
-//     inputField.value="";
-
-//     paragraph.addEventListener('dblclick', function(){
-//         toDoContainer.removeChild(paragraph);
-//     })
-// });
+AddToDoButton.addEventListener("click", function () {
+  var paragraph = document.createElement("p");
+  paragraph.classList.add("paragraph-styling");
+  paragraph.innerText = inputField.value;
+  toDoContainer.appendChild(paragraph);
+  inputField.value = "";
+  paragraph.addEventListener("dblclick", function () {
+    toDoContainer.removeChild(paragraph);
+  });
+});
 
 let addTodoBtn = document.getElementById("addTodoBtn");
 let modalElem = document.getElementById("modal");
@@ -24,26 +22,25 @@ addTodoBtn.addEventListener("click", showModal);
 closeBtn.addEventListener("click", hideModal);
 
 submitBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    let taskItem = document.getElementById("taskItem");
-    let taskDate = document.getElementById("taskDate");
-    saveTask([taskItem.value, taskDate.value]);
-    hideModal();
-        
+  event.preventDefault();
+  let taskItem = document.getElementById("taskItem");
+  let taskDate = document.getElementById("taskDate");
+  saveTask([taskItem.value, taskDate.value]);
+  hideModal();
 });
 
 function hideModal() {
-    modalElem.style.display = "none";
+  modalElem.style.display = "none";
 }
 
 function showModal() {
-    modalElem.style.display = "block";
+  modalElem.style.display = "block";
 }
 
 function saveTask(task) {
-    localStorage.setItem("task", [task]);
+  localStorage.setItem("task", [task]);
 }
 
 function getTask() {
-    return localStorage.getItem("task");
+  return localStorage.getItem("task");
 }
